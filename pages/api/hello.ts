0591b52f4dfd1e7bb7df0f5413deb446e2c2b57f
김태희 <evan@Taehees-MacBook-Pro.local>
2023-03-11 20:10:09 +0900

    First Commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const key = process.env.OPENAI_KEY;
+
+  const gptResult = await axios.post(
+    "https://api.openai.com/v1/completions",
+    {
+      model: "text-davinci-003",
+      prompt: req.body.prompt,
+      max_tokens: 100,
+      n: 1,
+      stop: null,
+      temperature: 0.7,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${key}`,
+      },
+    }
+  );
+
+  res.status(200).json({ result: gptResult.data.choices[0].text });
 }
